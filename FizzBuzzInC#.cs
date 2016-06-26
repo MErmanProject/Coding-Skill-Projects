8bf45308f0fa092f0dda7ef361f493c18e3a8660
MErmanProject <matter2014@yahoo.com>
2016-06-26 18:30:57 -0400

    FizzBuzzInC#

---
+++ FizzBuzzInC#.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FizzBuzzinC_Sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i=0; i<101; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+        }
+        private string FizzBuzz(int num) 
+        {
+            string mystring;
+            if (num % 5 == 0 && num % 3 == 0)
+            {
+                mystring = "FizzBuzz";
+            }
+            else if (num % 3 == 0)
+            {
+                mystring = "Fizz";
+            }
+            else if (num % 5 == 0)
+            {
+                mystring = "Buzz";
+            }
+            else
+                mystring = num.ToString();
+
+            return mystring;
+        }
+            
+
+    }
+}
