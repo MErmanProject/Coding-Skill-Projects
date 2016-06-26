c1c4dee92d664fa049d1ac07ec5c4a57ff0fbb17
MErmanProject <matter2014@yahoo.com>
2016-06-26 19:00:37 -0400

    FizzBuzzInPython

---
+++ FizzBuzzinPython.py
@@ -0,0 +1,15 @@
+def Initialize():
+    for i in range(1,101):
+        FizzBuzz(i)
+        
+def FizzBuzz(num):
+    if num % 5 == 0 and num % 3 == 0:
+        print "FizzBuzz"
+    elif num % 3 == 0:
+        print "Fizz"
+    elif num % 5 == 0:
+        print "Buzz"
+    else:
+        print num
+
+Initialize()
