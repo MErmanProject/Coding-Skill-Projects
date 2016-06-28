99a190379bdf74c9b360be14d0743d06c7b20fd0
MErmanProject <matter2014@yahoo.com>
2016-06-27 22:38:57 -0400

    Code Wars Highest Profit
    Description:
    
    Story
    
    Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
    
    Task
    
    Write a function that returns both the minimum and maximum number of the given list/array.
    
    Examples
    
    MinMax.minMax(new int[]{1,2,3,4,5}) == {1,5}
    MinMax.minMax(new int[]{2334454,5}) == {5, 2334454}
    MinMax.minMax(new int[]{1}) == {1, 1}
    Remarks
    
    All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.

---
+++ CodeWarsHighestProfit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HighestProfit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MinMax.minMax(new int[] { 1, 2, 4, 5, 3 });
+        }
+    }
+    public class MinMax
+    {
+        public static int[] minMax(int[] lst)
+        {
+            int min;
+            int max;
+
+            Array.Sort(lst); //sort the Array
+            min = lst[0]; //min will be the 1st index
+            max = lst[lst.Length - 1]; //max will be the last index
+
+            int[] result = { min, max }; //return an array of int {min,max}
+            return result;
+        }
+    }
+}
