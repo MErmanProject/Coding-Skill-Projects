0bf2686eac4c30f4c9303bcfb20b18277f5a1c75
MErmanProject <matter2014@yahoo.com>
2016-06-27 20:57:03 -0400

    Longest Consecutive Words Project in C#
    Description:
    
    You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
    
    Example:
    
    longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
    
    n being the length of the string array, if n = 0 or k > n or k <= 0 return "".

---
+++ LongestConsecutiveWords.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LongestConsecutiveWords
+{
+    public class LongestConsecutives
+    {
+
+        public static String LongestConsec(string[] strarr, int k)
+        {
+            int numtimes = k;
+            string challenge = "";
+            string longest = "";
+            int count = 0;
+            if (strarr.Length == 0 || numtimes == 0) //if the array is empty or consecutive words is 0 then do nothing
+            {
+
+            }
+            for (int i = 0; i < strarr.Length; i++) //cycle through the loop once
+            {
+                count = i;
+                if (numtimes <= strarr.Length - count) //cannot go past the last index of the array
+                {
+
+                    for (int x = 0; x < numtimes; x++) //cycle through the loop until you reach numtimes through
+                    {
+
+                        challenge += strarr[count]; //add in the next consecutive element from the array
+                        count++; //increment the count
+                    }
+                    if (challenge.Length > longest.Length) //if the challenge string length is greater than the longest string length
+                    {
+                        longest = challenge; //longest is updated
+                        challenge = "";
+                    }
+                    else
+                    {
+                        challenge = "";
+                    }
+                }
+            }
+            return longest;
+        }
+    }
+}
