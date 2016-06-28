660a58fce75b30c6dda1f6cac74ce34145c67243
MErmanProject <matter2014@yahoo.com>
2016-06-27 23:07:56 -0400

    Find The Capitals-Code Wars c#
    Instructions
    
    Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
    
    Example
    
    Assert.AreEqual(Kata.Capitals("CodEWaRs"), new int[]{0,3,4,6});

---
+++ FindTheCapitals.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindTheCapitals
+{
+    public partial class FindTheCapitals : Form
+    {
+        public FindTheCapitals()
+        {
+            InitializeComponent();
+            Kata.Capitals("CodEWaRs");
+        }
+
+
+  public static class Kata
+    {
+        public static int[] Capitals(string word)
+        {
+            int index = 0;
+            List<int> result = new List<int>(); //creates an empty list
+
+            foreach (char c in word) //cycles through each character
+            {
+                if (char.IsUpper(c)) //checks to see if the character is UpperCase
+                {
+                    result.Add(index); //if it is it adds the index to the list
+                }
+                index++;
+            }
+            return result.ToArray<int>(); //returns the list as an int []
+        }
+    }
+}
+}
