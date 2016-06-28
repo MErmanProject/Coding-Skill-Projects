81e8d1ad3211d255670c23bea4906a0e0d9c9469
MErmanProject <matter2014@yahoo.com>
2016-06-28 01:12:54 -0400

    CW_MovieTickets in c#
    The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollars bill. A "Avengers" ticket costs 25 dollars.
    
    Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
    
    Can Vasya sell a ticket to each person and give the change if he initially has no money and sells the tickets strictly in the order people follow in the line?
    
    Return YES, if Vasya can sell a ticket to each person and give the change. Otherwise return NO.
    
    Examples:
    
    // === C Sharp ===
    
    Line.Tickets(new int[] {25, 25, 50}) // => YES 
    Line.Tickets(new int[] {25, 100})  
               // => NO. Vasya will not have enough money to give change to 100 dollars

---
+++ Tickets.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_MovieTickets
+{
+    public partial class Tickets : Form
+    {
+        public Tickets()
+        {
+            InitializeComponent();
+            Line.Tickets(new int[] { 25, 25, 25, 25, 25, 100, 100 });
+        }
+
+        public class Line
+        {
+            public static string Tickets(int[] peopleInLine)
+            {
+               int[] change = new int[] { 0, 0, 0 };
+               foreach(int cust in peopleInLine)
+                {
+                    switch(cust)
+                    {
+                        case 25: //exact change, adds 1 25 in change
+                            change[0] += 1;
+                            break;
+
+                        case 50: //needs 25 in change
+                            if (change[0]<1)
+                            {
+                                return "NO";
+                            }
+                            else
+                            {
+                                change[0] -= 1;
+                                change[1] += 1;
+                            }
+                            break;
+
+                        case 100: //needs 75 in change
+                            if (change[0] < 3 || (change[0] < 1 && change[1] <1))
+                            {
+                                return "NO";
+                            }
+                            else
+                            {
+                                if(change[1] < 1) //no 50's, change must be 3 25's
+                                {
+                                    change[0] -= 3;
+                                }
+                                else
+                                {
+                                    change[0] -= 1; // 1 50 and 1 25 in change
+                                    change[1] -= 1;
+                                }
+                            }
+                            break;
+                    }
+                                          
+
+                }
+                return "YES";
+            }
+            
+        }
+    }
+}
