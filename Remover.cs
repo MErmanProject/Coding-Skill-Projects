0fc3b6d181eb641904ab3bf17f145255ad3f04e1
MErmanProject <matter2014@yahoo.com>
2016-06-27 23:58:39 -0400

    (CW)Remove the Minimum in C#
    The museum of incredible dull things
    
    The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
    
    However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
    
    Task
    
    Given an array of integers, remove the smallest value. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
    
    Don't change the order of the elements that are left.
    
    Examples
    
    Remover.RemoveSmallest(new List<int>{1,2,3,4,5}) = new List<int>{2,3,4,5}
    Remover.RemoveSmallest(new List<int>{5,3,2,1,4}) = new List<int>{5,3,2,4}
    Remover.RemoveSmallest(new List<int>{2,2,1,2,1}) = new List<int>{2,

---
+++ Remover.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Remover
+{
+    public partial class Remover : Form
+    {
+        public Remover()
+        {
+            InitializeComponent();
+
+            RemoveSmallest(new List<int> { 2, 2, 1, 2, 1 });
+        }
+            public static List<int> RemoveSmallest(List<int> numbers)
+            {
+                List<int> myList = new List<int>();
+                int index = 0;
+                myList = numbers;
+
+                if (myList.Count > 0) //only remove index if list has elements
+                {
+                    index = myList.IndexOf(myList.Min()); //sets the index to the first minimum element
+                    myList.RemoveAt(index);   //removes the index of the minimum element         
+                }
+                return myList; //returns the list
+            }
+    }
+
+}
+
+
+
+
+
