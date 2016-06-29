8cbbc8bedc6d224142a77ddd3518370edb94869e
MErmanProject <matter2014@yahoo.com>
2016-06-28 22:42:11 -0400

    CW_JosephusSurvivor(recursively) in C#
    Description:
    
    This problem takes its name by arguably the most important event in the life of the ancient historian Josephus: according to his tale, he and his 40 soldiers were trapped in a cave by the Romans during a siege.
    
    Refusing to surrender to the enemy, they instead opted for mass suicide, with a twist: they formed a circle and proceeded to kill one man every three, until one last man was left (and that it was supposed to kill himself to end the act).
    
    Well, Josephus and another man were the last two and, as we now know every detail of the story, you may have correctly guessed that they didn't exactly follow through the original idea.
    
    You are now to create a function that returns a Josephus permutation, taking as parameters the initial array/list of items to be permuted as if they were in a circle and counted out every k places until none remained.
    
    Tips and notes: it helps to start counting from 1 up to n, instead of the usual range 0..n-1; k will al

---
+++ JosephusSurvivor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_JosephusSurvivor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var myint = 0;
+            myint=JosephusSurvivor.JosSurvivor(56, 7);
+        }
+    }
+    public class JosephusSurvivor
+    {
+        public static int JosSurvivor(int n, int k)
+        {
+
+            if (n == 1) //base case
+                return 1;
+            else //call a recursive function based on the mathematical principles involved.
+
+                return (JosSurvivor(n - 1, k) + k - 1) % n + 1;
+
+        }
+    }
+}
