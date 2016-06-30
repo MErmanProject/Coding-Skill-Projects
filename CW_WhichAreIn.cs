d4ae1f15e905115d16fa2aafebe55e38aac107fb
MErmanProject <matter2014@yahoo.com>
2016-06-30 02:37:28 -0400

    CW_Which Are In? in C#
    Description:
    
    Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order and without duplicates of the strings of a1 which are substrings of strings of a2.
    
    Example 1:
    
    a1 = ["arp", "live", "strong"]
    
    a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
    
    returns ["arp", "live", "strong"]
    
    Example 2:
    
    a1 = ["tarp", "mice", "bull"]
    
    a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
    
    returns []
    
    Notes:
    
    Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
    
    Beware: r must be without duplicates.

---
+++ CW_WhichAreIn.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_WhichAreIn
+{
+    public partial class CW_WhichAreIn : Form
+    {
+        public CW_WhichAreIn()
+        {
+            InitializeComponent();
+            string[] a1 = new string[] { "arp", "live", "strong" };
+            string[] a2 = new string[] { "lively", "alive", "harp", "sharp", "armstrong" };
+
+            WhichAreIn.inArray(a1, a2);
+        }
+
+        class WhichAreIn
+        {
+            public static string[] inArray(string[] array1, string[] array2)
+            {
+                var myint = array1.Length;
+                var result = new List<string>();
+
+                var count = 0;
+
+                foreach (string s in array1) //cycles through each string in array1
+                {
+                    foreach (string s1 in array2) //cycles through each string in array2
+                    {
+                        if (s1.Contains(s) && !result.Contains(s)) //if s is in s1 and not in result then add it to the list
+                            result.Add(s);
+
+                    }
+                    count++;
+                }
+                string[] resultStrArr = result.ToArray(); //use a list to automatically size the array to avoid NULL values
+
+                Array.Sort(resultStrArr); //sorts the array lexographically
+
+                return resultStrArr;
+            }
+        }
+    }
+}
