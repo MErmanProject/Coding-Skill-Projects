453f59f775018eb42a36acb90007e1a8f3536b90
MErmanProject <matter2014@yahoo.com>
2016-06-30 02:04:28 -0400

    CW_Pizza Cuts in C#
    Description
    
    In her trip to Italy, Elizabeth Gilbert made it her duty to eat perfect pizza. One day, she ordered one for dinner. And then some Italian friends appeared at her room.
    
    The problem is that there were many people who ask for a piece of pizza at that moment. And she had a knife that only cuts straight.
    
    Given a number K (K<=45000), help her get the maximum of pieces possible (not necessarily of equal size) with K cuts. If K is a negative number, the result must be -1 (or Nothing in Haskell).
    
    Examples
    
    maxPizza(0) == 1
    maxPizza(1) == 2
    maxPizza(3) == 7

---
+++ CW_PizzaCuts.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_PizzaCuts
+{
+    public partial class CW_PizzaCuts : Form
+    {
+        public CW_PizzaCuts()
+        {
+            InitializeComponent();
+            Console.WriteLine(Pizza.maxPizza(4));
+        }
+
+        public class Pizza
+        {
+
+            public static int maxPizza(int cut)
+            {
+                switch (cut)
+                {
+                    case 0:               
+                       return 1;  //no cuts max is 1                                              
+                    default:
+                        if (cut < 0)
+                            return -1; //a negative number returns -1
+                        else
+                            return ((cut * cut) + cut + 2) / 2;  //mathematical formula to get most possible cuts                   
+                }
+            }
+        }
+    }
+}
