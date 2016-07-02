d0aec718e3fac467d341f00c384c763e1a168f5a
MErmanProject <matter2014@yahoo.com>
2016-07-02 01:23:21 -0400

    CW_PatternCraft-State in C#
    Description:
    
    The State Design Pattern can be used, for example, to manage the state of a tank in the StarCraft game.
    
    The pattern consists in isolating the state logic in different classes rather than having multiple ifs to determine what should happen.
    
    Your Task
    
    Complete the code so that when a Tank goes into SiegeMode it cannot move and its damage goes to 20. When it goes to TankMode it should be able to move and the damage should be set to 5.
    
    You have 3 classes:
    
    Tank: has a state, canMove and damage properties
    SiegeState and TankState: has canMove and damage properties
    Note: The tank initial state should be TankState.
    
    Resouces
    
    PatternCraft > State
    SourceMaking > State
    Wikipedia > State
    PatternCraft series
    
    State Pattern
    Strategy Pattern
    Visitor Pattern
    Decorator Pattern
    Adapter Pattern
    Command Pattern
    The original PatternCraft series (by John Lindquist) is a collection of Youtube videos that explains some of the design patterns and how they a

---
+++ CW_PatternCraft-State.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CW_PatternCraft_State
+{
+    public delegate void ChangedEventHandler(object sender, EventArgs e);
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            IUnit tank =  new Tank();
+            tank.State = new SiegeState();
+            Console.WriteLine(tank.State);
+            Console.WriteLine(tank.CanMove);
+            Console.WriteLine(tank.Damage);
+
+        }
+    }
+
+    public interface IUnit
+    {
+
+        IUnitState State { get; set; }
+        bool CanMove { get; }
+        int Damage { get; }
+    }
+
+    public interface IUnitState
+    {
+        bool CanMove { get; set; }
+        int Damage { get; set; }
+    }
+
+    public class SiegeState : IUnitState
+    {
+        private bool _canmove;
+        private int _damage;
+        Tank mytank = new Tank();
+
+        public SiegeState()
+        {          
+            _canmove = false;
+            _damage = 20;
+         }
+        private void StateChanged(object sender, EventArgs e)
+        {
+            mytank.State = this;
+            mytank.CanMove = false;
+            mytank.Damage = 20;
+        }
+
+        #region "Properties"
+        public bool CanMove
+        {
+            get { return _canmove; }
+            set { _canmove = value; }
+        }
+        public int Damage
+        {
+            get { return _damage; }
+            set { _damage = value; }
+        }
+        #endregion
+    }
+
+    public class TankState : IUnitState
+    {
+        
+        private bool _canmove;
+        private int _damage;
+     
+        public TankState()
+        {
+
+            _canmove = true;
+            _damage = 5;
+
+        }
+
+        #region "Properties"
+        public bool CanMove
+        {
+            get { return _canmove; }
+            set { _canmove = value; }
+        }
+        public int Damage
+        {
+            get { return _damage; }
+            set { _damage = value; }
+        }
+        #endregion
+    }
+
+    public  class Tank :  IUnit
+    {
+   
+        private static bool _canmove;
+        private static int _damage;
+        private static IUnitState _state;
+        public Tank()
+        { 
+            _state = new TankState();
+            _canmove = _state.CanMove;
+            _damage = _state.Damage;
+        }
+
+        #region "Properties"
+        public bool CanMove
+        {
+            get { return _state.CanMove; }
+            set { _state.CanMove = value; }
+        }
+        public int Damage
+        {
+            get { return _state.Damage; }
+            set { _state.Damage = value; }
+        }
+        public IUnitState State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        #endregion
+
+    }
+
+}
