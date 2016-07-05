68d29749d2fc3956343a40eab33baf35cd862f69
MErmanProject <matter2014@yahoo.com>
2016-07-05 01:09:31 -0400

    CW_Sum Strings in C#
    Given the string representations of two integers, return the string representation of the sum of those integers.
    
    For example:
    
    sumStrings('1','2') // => '3'
    C# sumStrings("1","2") // => "3"
    
    A string representation of an integer will contain no characters besides the ten numerals "0" to "9"

---
+++ CW_Sum Strings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace CW_Sum_Strings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+             
+            Kata.sumStrings("", "5");
+        }
+    }
+
+
+    public static class Kata
+    {
+        public static string sumStrings(string a, string b)
+        {
+            if (a.Length == 0)
+                return b;  //returns the string itself if one of them is empty
+            if (b.Length == 0)
+                return a;
+
+            var big1 = BigInteger.Parse(a); //otherwise, use the built in BigInteger class in System.Numerics
+            var big2 = BigInteger.Parse(b); //parse each to a BigInteger
+            var mybig = big1 + big2; //add together
+
+            return mybig.ToString(); //return as a string
+        }
+    }
+}
