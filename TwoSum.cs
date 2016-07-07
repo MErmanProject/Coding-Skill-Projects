85ec38c06d17e82163390bd2726fbd1757e990d0
MErmanProject <matter2014@yahoo.com>
2016-07-06 23:51:41 -0400

    CW_TwoSum in C#
    Write a function that, given a list and a target sum, returns zero-based indices of any two distinct elements whose sum is equal to the target sum. If there are no such elements, the function should return null.
    
    For example, FindTwoSum(new List<int>() { 1, 3, 5, 7, 9 }, 12) should return any of the following tuples of indices:
    
    1, 4 (3 + 9 = 12)
    2, 3 (5 + 7 = 12)
    3, 2 (7 + 5 = 12)
    4, 1 (9 + 3 = 12)

---
+++ TwoSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoSum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Tuple<int, int> indices = TwoSum.FindTwoSum(new List<int>() { 1, 3, 5, 2, 4, 5, 8, 5, 7, 6, 5, 54 }, 61);
+            Console.WriteLine(indices.Item1 + " " + indices.Item2);
+        }
+    }
+
+    class TwoSum
+    {
+        public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
+        {
+            //var secondint=0;
+            var listpos = 0;
+            var count = 0;
+            var myint1 = 0;
+            var myint2 = 0;
+            var mysol = //use linq to create 2 variables to cycle through the list, then use a lamba expression to get the appropriate values
+               list
+               .SelectMany((mynum1) => list.Select((mynum2) => new { n1 = mynum1, n2 = mynum2}))
+               .Where(a => a.n1 + a.n2 == sum );
+
+            foreach (var x in mysol) //get the variables for ease of use
+            {
+                myint1 = x.n1;
+                myint2 = x.n2;
+            }
+
+            var mytuple = new Tuple<int, int>(myint1, myint2); //since Tuple is immutable, it must be initiated with the appropriate value
+            return mytuple;
+        }
+    }
+}
+
