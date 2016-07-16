3947414b4e983087ebcb60e340a1b7f2067a4e31
MErmanProject <matter2014@yahoo.com>
2016-07-15 23:59:04 -0400

    CE_StackInterface
    CodeEval Challenge---Implement a Stack Interface.  Received a score 63.792/65 based off of correctness, memory usage and time taken
    
    DESCRIPTION:
    Write a program which implements a stack interface for integers. The interface should have ‘push’ and ‘pop’ functions. Your task is to ‘push’ a series of integers and then ‘pop’ and print every alternate integer.

---
+++ Stack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StackImplementation
+{
+    public partial class Stack : Form
+    {
+        public Stack()
+        {
+            InitializeComponent();
+            string[] myarray = { "0", "2", "3", "4", "5", "6", "7", "8", "9", "12", "43", "15", "25" };
+            var MyImp = new MyInterfaceImplementation();
+
+            MyImp.GetFileContent(myarray);
+        }
+    }
+
+    public interface MyStack
+    {
+        void Push(string[] mystring);
+
+        string Pop();
+    }
+
+    public class MyInterfaceImplementation : MyStack
+    {
+        private Stack<string> mystack = new Stack<string>();
+
+        public void GetFileContent(string[] myarray)
+        {
+            Push(myarray);
+            Console.WriteLine(Pop());
+        }
+
+        public string Pop()
+        {
+            string mystring = "";
+            int count = 0;
+            int stackcount = mystack.Count();
+            for (int i = 0; i < stackcount; i++)
+            {
+                if (count % 2 == 0)
+                    mystring += mystack.Pop() + " ";
+                else
+                    mystack.Pop();
+                count++;
+            }
+            return mystring;
+        }
+
+        public void Push(string[] mystring)
+        {
+            foreach (var str in mystring)
+            {
+                mystack.Push(str);
+            }
+        }
+    }
+}
