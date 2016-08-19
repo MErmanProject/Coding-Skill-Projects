448c6e6af840009d441ea9cb239a70e4b44ba835
MErmanProject <matter2014@yahoo.com>
2016-08-19 16:29:16 -0400

    CE_Multiply Lists
    CHALLENGE DESCRIPTION:
    
    You have 2 lists of positive integers. Write a program which multiplies corresponding elements in these lists.
    
    INPUT SAMPLE:
    
    Your program should accept as its first argument a path to a filename. Input example is the following
    
    9 0 6 | 15 14 9
    5 | 8
    13 4 15 1 15 5 | 1 4 15 14 8 2
    The lists are separated with a pipe char, numbers are separated with a space char. 
    The number of elements in lists are in range [1, 10]. 
    The number of elements is the same in both lists. 
    Each element is a number in range [0, 99].
    
    OUTPUT SAMPLE:
    
    Print the result in the following way.
    
    135 0 54
    40
    13 16 225 14 120 10

---
+++ Multiply Lists.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiply_Lists
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var str = "41 21 41 81 76 | 42 40 40 32 50";
+
+            var myarr = str.Split(' ');
+
+            var arr1 = myarr.TakeWhile(a => String.Compare("|", a, true) != 0);
+            var arr2 = myarr.SkipWhile(a => String.Compare("|", a, true) != 0).Where(a => a != '|'.ToString());
+
+            str = "";
+
+            var mult = arr1.Zip(arr2, (s, i) => int.Parse(s) * int.Parse(i) + " ");
+
+            foreach (var m in mult)
+            {
+                str += m;
+            }
+
+            str = str.Remove(str.Length - 1);
+            Console.WriteLine(str);
+        }
+    }
+}
