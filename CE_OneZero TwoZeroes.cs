761e662d1c84f809df409cb9b15905e703dfc4ea
MErmanProject <matter2014@yahoo.com>
2016-08-27 16:55:09 -0400

    CE_OneZero_TwoZeroes
    For example, for the given numbers 2 and 4, you convert all numbers from 1 to 4 inclusive into the binary system. As a result, you get 1, 10, 11, and 100. As the first given number is 2, this means that we are looking for numbers with two zeros, so only 100 suits us. Hence, the result will be 1: there is only one number with two zeros.
    
    INPUT SAMPLE:
    
    The first argument is a path to a file. Each line includes a test case with two numbers: the first one is the number of zeros in a binary code that we need to find and the second one is the range from 1 to this number where you have to find these zeros.
    
    For example:
    
    
    
    1
    2
    1 8
    2 4
    OUTPUT SAMPLE:
    
    Print the total number of numerals that contain the needed amount of zeros in a binary system.
    
    For example:
    
    
    
    1
    2
    3
    1
    CONSTRAINTS:
    
    Range can be from 5 to 1000.
    Number of zeros does not exceed the length of binary code number.
    The number of test cases is 40.

---
+++ CE_OneZero TwoZeroes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CE_OneZero_Two_Zeros
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var line = "1 8";
+            var myarr = line.Split(' ').Select(int.Parse);
+            var mylist = new List<string>();
+            var count = 0;
+
+            for (int i = 0; i < myarr.ElementAt(1); i++)
+            {
+                mylist.Add(Convert.ToString(i + 1, 2));
+            }
+
+            foreach (string str in mylist)
+            {
+                var res = str.GroupBy(a => a).Where(a => a.Key == '0').OrderBy(a => a.Key);
+                if (res.Count() != 0 && res.ElementAt(0).Count() == myarr.ElementAt(0))
+                    count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
