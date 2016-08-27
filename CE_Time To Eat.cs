31b724db17a2d101ba6983a18e76067dce578c60
MErmanProject <matter2014@yahoo.com>
2016-08-27 16:53:20 -0400

    CE_Time To Eat
    At codeeval.com, we have a cat called Kitty. She loves eating, and it seems like she can eat anything any time. We want to keep Kitty fit, so we feed her according to a schedule. 
    Planning her daily meals, we need to see when Kitty will eat starting with the latest meal in the evening to the earliest morning snack. So, your task is to sort timestamps in the schedule in a reverse chronological order.
    
    INPUT SAMPLE:
    
    The first argument is a path to a file. Each line includes a test case: a schedule containing unsorted timestamps in HH:MM:SS format.
    
    For example:
    
    
    
    1
    2
    02:26:31 14:44:45 09:53:27
    05:33:44 21:25:41
    OUTPUT SAMPLE:
    
    Sort timestamps in each schedule from the biggest to the smallest one.
    
    
    
    1
    2
    14:44:45 09:53:27 02:26:31
    21:25:41 05:33:44
    CONSTRAINTS:
    
    Each schedule may have from 2 to 20 timestamps.
    Timestamp 23:59:59 - biggest and 00:00:01 the smallest one.
    The number of test cases is 40.

---
+++ CE_Time To Eat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CE_Time_To_Eat
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var line = "02:26:31 14:44:45 09:53:27";
+            var str = "";
+            var arr = line.Split(' ');
+            var res = arr.OrderByDescending(a => a);
+
+            foreach (var time in res)
+            {
+                str += time + " ";
+            }
+
+            str = str.Trim();
+
+            Console.WriteLine(str);
+        }
+    }
+}
