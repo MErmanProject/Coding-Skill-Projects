d18e443bf22252a7777257a37486f852233dc6a5
MErmanProject <matter2014@yahoo.com>
2016-08-27 16:51:35 -0400

    CE_Black Card
    You must have heard about pirates, their customs, pirates code, and the “black spot”. If a pirate is presented with a “black spot”, he is officially pronounced guilty, meaning he will soon be expulsed from the pirate brotherhood or even be dead. 
    We don’t have as strict rules as pirates have, and a person who receives a black spot simply leaves the game. 
    For example, we have a list of three players: John, Tom, Mary, and a number 5. Starting with the first player (in our case, it’s John), we start to count all players: John – 1, Tom – 2, Mary – 3, and then again starting from the first one John – 4, Tom – 5. As Tom gets number 5, he should leave. Now, we have John and Mary and start counting again. John gets number 5, so he leaves. Thus, the winner is Mary.
    
    INPUT SAMPLE:
    
    The first argument is a path to a file. Each line includes a test case with names of players and a number for a “black spot”. Players and a number are separated by a pipeline '|'.
    
    For exampl

---
+++ CE_BlackCard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CE_Black_Card
+{
+    internal class CE_BlackCard
+    {
+        private static void Main(string[] args)
+        {
+            var line = "Zacf Hzzemo Pixud Zeuvbe Zwek Yop | 15";
+            var arr = line.Split('|').ToList();
+            var num = int.Parse(arr[arr.Count - 1]);
+            var index = 0;
+            arr.RemoveAt(arr.Count - 1);
+            var arr1 = arr[0].Split(' ').ToList();
+            arr1.Remove("");
+
+            Console.WriteLine(GetName(arr1, num, index));
+        }
+
+        private static string GetName(List<String> arr1, int num, int index)
+        {
+            if (arr1.Count() == 1)
+                return arr1[0].ToString();
+
+            for (int i = 0; i < num; i++)
+            {
+                if (index > arr1.Count() - 1)
+                    index = 0;
+
+                index++;
+
+                if (i == num - 1)
+                {
+                    arr1.RemoveAt(index - 1);
+                    index = 0;
+                    GetName(arr1, num, index);
+                }
+            }
+
+            return arr1[0].ToString();
+        }
+    }
+}
